7efbbef1d4e3115796d0e2faa2099d88913a4aaf
Matheus Ferreira <mattferreira.dev@protonmail.com>
2024-03-25 21:33:48 -0300

    create `queue` data-structure

---
+++ queue.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+struct Queue {
+  private:
+    unsigned capacity;
+    int front;
+    int* items;
+    unsigned size = 0;
+    int tail;
+
+  public:
+    Queue(unsigned int c): capacity(c) {
+      this->items = new int[c];
+    }
+
+    void clear() {
+      for (int i = 0; i < this->size; i++) {
+        this->items[i] = NULL;
+
+        for (int j = i; j < this->size; j++) {
+          this->items[j] = NULL;
+        }
+      }
+    }
+
+    int dequeue() {
+      if (this->empty()) {
+        return NULL;
+      }
+
+      this->front = this->items[1];
+
+      for (int i = 0; i < this->size; i++) {
+        this->items[i] = this->items[i + 1];
+      }
+
+      return this->front;
+    }
+
+    bool empty() {
+      return this->size == 0;
+    }
+
+    void enqueue(int item) {
+      if (this->full()) {
+        throw runtime_error("[Queue]: is full!");
+      }
+
+      this->tail = item;
+      this->items[this->size] = item;
+
+      this->size++;
+    }
+
+    int front() {
+      return this->front;
+    }
+
+    bool full() {
+      return this->size == this->capacity;
+    }
+
+    void show() {
+      for (int i = 0; i < this->size; i++) {
+        cout << "Item: " << this->items[i] << " | Position: " << i << endl;
+      }
+    }
+
+    int size() { 
+      return this->size;
+    }
+
+    int tail() {
+      return this->tail;
+    }
+};
+
+int main() {
+  return 0;
+}
