062dd72f1225c8abee4a22e5271f4ce81026e884
Matheus Ferreira <mattferreira.dev@protonmail.com>
2024-05-14 17:51:40 -0300

    create golang implementation of stack

---
+++ go/stack.go
@@ -0,0 +1,68 @@
+package stack
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Stack struct {
+	items []*int64
+	capacity,
+	lastIndex,
+	size uint64
+}
+
+func NewStack(capacity uint64) Stack {
+	return Stack{
+		capacity:  capacity,
+		items:     []*int64{},
+		lastIndex: 0,
+		size:      0,
+	}
+}
+
+func (s Stack) clear() {
+	if s.empty() {
+		errors.New("[stack] is empty!")
+	}
+
+	var i = s.size - 1
+
+	for i != 0 {
+		s.items[i] = nil
+	}
+
+	s.size = 0
+	s.lastIndex = 0
+}
+
+func (s Stack) empty() bool {
+	return s.size == 0
+}
+
+func (s Stack) full() bool {
+	return s.size == s.capacity
+}
+
+func (s Stack) insert(item *int64) {
+	if s.full() {
+		errors.New("[stack] is full!")
+	}
+
+	s.lastIndex++
+	s.items[s.lastIndex] = item
+}
+
+func (s Stack) pop() {}
+
+func (s Stack) show() {
+	var i = s.size - 1
+
+	for i != 0 {
+		fmt.Println(s.items[i])
+	}
+}
+
+func (s Stack) top() *int64 {
+	return s.items[s.lastIndex]
+}
