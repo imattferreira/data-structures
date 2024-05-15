be89a7b3f5532527d4d0fd8ea6addc14f3c9fa39
Matheus Ferreira <mattferreira.dev@protonmail.com>
2024-05-15 14:55:25 -0300

    create rust implementation of doubly linked list

--- rust/src/doubly_linked_list.rs
+++ rust/src/doubly_linked_list.rs
@@ -1,9 +1,102 @@
-// TODO: finish implementation (use pointers instead of Boxes??)
+use std::ptr;
 
-struct Node {}
+struct Node {
+    value: i64,
+    next: *mut Node,
+    previous: *mut Node,
+}
 
-impl Node {}
+impl Node {
+    pub fn new(value: i64) -> Self {
+        Self {
+            value,
+            next: ptr::null_mut(),
+            previous: ptr::null_mut(),
+        }
+    }
+}
 
-struct DoublyLinkedList {}
+struct DoublyLinkedList {
+    size: u64,
+    head: *mut Node,
+}
 
-impl DoublyLinkedList {}
+impl DoublyLinkedList {
+    pub fn new() -> Self {
+        Self {
+            size: 0,
+            head: ptr::null_mut(),
+        }
+    }
+
+    pub fn insert(&mut self, value: i64) {
+        let node = Box::into_raw(Box::new(Node::new(value)));
+        self.size += 1;
+
+        if self.head.is_null() {
+            self.head = node;
+            return;
+        }
+
+        let mut tmp = self.head;
+
+        unsafe {
+            while !tmp.is_null() {
+                if (*tmp).next.is_null() {
+                    (*node).previous = tmp;
+                    (*tmp).next = node;
+                    break;
+                }
+
+                tmp = (*tmp).next;
+            }
+        }
+    }
+
+    pub fn insertAtBeginning(&mut self, value: i64) {
+        let node = Box::into_raw(Box::new(Node::new(value)));
+        self.size += 1;
+
+        if self.head.is_null() {
+            self.head = node;
+            return;
+        }
+
+        unsafe {
+            (*node).next = self.head;
+            (*self.head).previous = node;
+            self.head = node;
+        }
+    }
+
+    pub fn pop(&mut self) {
+        if self.head.is_null() {
+            return;
+        }
+
+        self.size -= 1;
+        let mut tmp = self.head;
+
+        unsafe {
+            while !tmp.is_null() {
+                if (*tmp).next.is_null() {
+                    (*(*tmp).previous).next = ptr::null_mut();
+                    break;
+                }
+
+                tmp = (*tmp).next;
+            }
+        }
+    }
+
+    pub fn show(&self) {
+        let mut tmp = self.head;
+
+        unsafe {
+            while !tmp.is_null() {
+                println!("{}", (*tmp).value);
+                tmp = (*tmp).next;
+            }
+        }
+    }
+}
--- rust/src/linked_list.rs
+++ rust/src/linked_list.rs
@@ -1,9 +1,102 @@
-// TODO: finish implementation (use pointers instead of Boxes??)
+use std::ptr;
 
-struct Node {}
+struct Node {
+    value: i64,
+    next: *mut Node,
+}
 
-impl Node {}
+impl Node {
+    pub fn new(value: i64) -> Self {
+        Self {
+            value,
+            next: ptr::null_mut(),
+        }
+    }
+}
 
-struct LinkedList {}
+struct LinkedList {
+    size: u64,
+    head: *mut Node,
+}
 
-impl LinkedList {}
+impl LinkedList {
+    pub fn new() -> Self {
+        Self {
+            size: 0,
+            head: ptr::null_mut(),
+        }
+    }
+
+    pub fn insert(&mut self, value: i64) {
+        let node = Box::into_raw(Box::new(Node::new(value)));
+        self.size += 1;
+
+        if self.head.is_null() {
+            self.head = node;
+            return;
+        }
+
+        let mut tmp = self.head;
+
+        unsafe {
+            while !tmp.is_null() {
+                if (*tmp).next.is_null() {
+                    (*tmp).next = node;
+                    break;
+                }
+
+                tmp = (*tmp).next;
+            }
+        }
+    }
+
+    pub fn insertAtBeginning(&mut self, value: i64) {
+        let node = Box::into_raw(Box::new(Node::new(value)));
+        self.size += 1;
+
+        let mut tmp = self.head;
+
+        unsafe {
+            while !tmp.is_null() {
+                if (*tmp).next.is_null() {
+                    (*tmp).next = node;
+                    break;
+                }
+
+                tmp = (*tmp).next;
+            }
+        }
+    }
+
+    pub fn pop(&mut self) {
+        if self.head.is_null() {
+            return;
+        }
+
+        let mut previous = self.head;
+        let mut current = self.head;
+
+        unsafe {
+            while !current.is_null() {
+                if (*current).next.is_null() {
+                    (*previous).next = ptr::null_mut();
+                    break;
+                }
+
+                previous = current;
+                current = (*current).next;
+            }
+        }
+    }
+
+    pub fn show(&self) {
+        let mut tmp = self.head;
+
+        unsafe {
+            while !tmp.is_null() {
+                println!("{}", (*tmp).value);
+                tmp = (*tmp).next;
+            }
+        }
+    }
+}
